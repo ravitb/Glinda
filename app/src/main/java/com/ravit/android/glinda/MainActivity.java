b4a9d79db992c2b43141fa5d568f42fa86d24be9
ravitb <ravit@rounds.com>
2016-03-11 17:06:49 +0200

    Integration

--- app/src/main/java/com/ravit/android/glinda/MainActivity.java
+++ app/src/main/java/com/ravit/android/glinda/MainActivity.java
@@ -7,9 +7,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,23 +27,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+				Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		Button btn  = (Button) findViewById(R.id.button);
+		btn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Editable num = ((EditText) findViewById(R.id.editText)).getText();
+				Snackbar.make(view, num + " was added to your watch list", Snackbar.LENGTH_LONG)
 						.setAction("Action", null).show();
 			}
 		});
-/*
-		if (mGoogleApiClient == null) {
-			mGoogleApiClient = new GoogleApiClient.Builder(this)
-					.addConnectionCallbacks(this)
-					.addOnConnectionFailedListener(this)
-					.addApi(LocationServices.API)
-					.build();
-		}*/
-
 		IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
 		filter.addAction(Intent.ACTION_SCREEN_OFF);
 		mGlindaReceiver = new GlindaReceiver();
@@ -78,4 +83,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+	public void submitNewNumber(View view) {
+		Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show();
+	}
 }
